47242642ccc869c5c109d049356375ac5cfc6afd
Saffan Ahmed <saffan_a@icloud.com>
2021-03-05 03:08:39 +0000

    creating class method.

--- tsdr_cnn_model.py
+++ tsdr_cnn_model.py
@@ -11,3 +11,13 @@ from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Conv2D
+
+# TSDR CNN Model Class
+
+
+class TSDR_Model:
+    @staticmethod
+    def build_model(width, height, depth, classes):
+        cnnModel = Sequential()
+        inputShape = (height, width, depth)
+        channelDim = -1
