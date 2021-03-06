345d9961a23f9d45bde525d73a246a6a20290d67
Saffan Ahmed <saffan_a@icloud.com>
2021-03-06 01:54:29 +0000

    Proposed Plan for Training Script.

---
+++ train.py
@@ -0,0 +1,16 @@
+# Author - Saffan Ahmed
+
+# Training Script:
+
+"""
+
+Purpose of training script will be used to train the CNN Model for the TSDR_Dataset.
+
+Goals for this script:
+1 - Load the training and testing split from the TSDR_Dataset.
+2 - Preprocessing the images to improve clarity and avoid class skew.
+3 - Training the CNN model with TSDR_Dataset.
+4 - Evaluate the accuracy of the CNN Model / Output.
+5 - Store the CNN model to personal computer disk-space to make predictions for new Traffic Sign Inputs.
+
+"""
