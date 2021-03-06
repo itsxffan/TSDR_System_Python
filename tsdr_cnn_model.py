0a3ca91399674f6a665165a81b568b3c38302477
Saffan Ahmed <saffan_a@icloud.com>
2021-03-06 01:44:35 +0000

    Fully Connected Layers Add for First & Second Set.

--- tsdr_cnn_model.py
+++ tsdr_cnn_model.py
@@ -45,3 +45,22 @@ class TSDR_Model:
         model.add(Activation("relu"))
         model.add(BatchNormalization(axis=chanDim))
         model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        # First Set of Fully Connected >> RELU (default acitivation function that shoots the neuron) layers
+        model.add(Flatten())
+        model.add(Dense(128))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        # Second Set of Fully Connected >> RELU (default acitivation function that shoots the neuron) layers
+        model.add(Flatten())
+        model.add(Dense(128))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        # SoftMax Classfifiers
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+        # return the constructed network architecture
+
+        return model
